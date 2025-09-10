dca03605e02aac90f8f8efc5e96eabd8503c0fff
vanshgupta11 <vanshkumargupta11@gmail.com>
2025-09-10 18:27:05 +0530

    latest

--- protfolio/src/App.jsx
+++ protfolio/src/App.jsx
@@ -2,17 +2,19 @@ import React, { useEffect, useState } from 'react'
 import Loader from './Components/Loader'
 
 function App() {
-const [isloading , setLoading] = useState(true)
-useEffect(()=>{
-    const fake =()=>{
-        setTimeout(()=>{
-            setLoading(false);
-        },4000)
-    }
-    fake();
-},[])
+// const [isloading , setLoading] = useState(true)
+// useEffect(()=>{
+//     const fake =()=>{
+//         setTimeout(()=>{
+//             setLoading(false);
+//         },4000)
+//     }
+//     fake();
+// },[])
   return (
-    <div>{isloading?(<Loader />):(<div>hello</div>)}</div>
+    <main>
+      <h1 className="text-5xl text-orange-500 font-bold font-circular-web">hello</h1>
+    </main>
   )
 }
 
---
+++ protfolio/tailwind.config.js
@@ -0,0 +1,17 @@
+
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+          zentry: ["zentry", "sanf-serif"],
+          general: ["general", "sanf-serif"],
+          'circular-web': ["circular-web", "sanf-serif"],
+          'robert-medium': ["robert-medium", "sanf-serif"],
+          'robert-regular': ["robert-regular", "sanf-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
