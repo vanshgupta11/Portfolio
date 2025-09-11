e97ac7793fa74df6229be605a779bba149c66c13
vanshgupta11 <vanshkumargupta11@gmail.com>
2025-09-11 16:29:19 +0530

    port

--- protfolio/src/Components/Navbar.jsx
+++ protfolio/src/Components/Navbar.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='border-amber-300 border-4 h-[10vh]'>
-                navbar
+    <div className='border-amber-900 border-4 h-[10vh] text-white flex items-center'>
+        <div className='flex gap-10'>
+        {["home","about","project","resume"].map((item,index)=>
+                   <a key={index} className={`'text-md font-light capitalize' ${index===3 && 'ml-32'}`}>{item}</a>)}
+        </div>
+                
     </div>
   )
 }
